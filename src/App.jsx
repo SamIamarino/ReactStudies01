ccc44ad08d9c3327356fba52b71090e0182af185
Samuel Ferla Iamarino <samuelf.iamarino@gmail.com>
2024-02-14 13:47:38 -0300

    Another method to display the tabContents component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,30 @@ import { CORE_CONCEPTS } from './data';
 import { EXAMPLES } from './data';
 
 function App() {
-  const [selectedTopic,setSelectedTopic] = useState('components');
+  const [selectedTopic,setSelectedTopic] = useState('');
 
   function handleSelect(selectedButton){
     //selectedButton => 'components','jsx','props','state'
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
 }   
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if(selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code} 
+              </code>
+            </pre>
+          </div>
+    );
+  }
+
   return (
     <div>
       <Header/>
@@ -37,15 +54,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code} 
-              </code>
-            </pre>
-          </div>
+          {tabContent}
           
         </section>
         
